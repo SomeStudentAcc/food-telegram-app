d1d65f0543bf9c4581e86b0981df6974ca958b56
AbdullohAA <abdullohabduganiev55555@gmail.com>
2025-03-01 15:00:18 +0500

    added header

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 
-export default function Button({props}) {
+export default function Button(props) {
   return (
     <button onClick={props.onClick} className={'button '+ props.className}>
         close
